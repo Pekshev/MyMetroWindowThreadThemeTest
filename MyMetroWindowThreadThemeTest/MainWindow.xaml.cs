6cff40dec0433d3b3bc870987fb908a038b88e4c
Modis <cjmodis@gmail.com>
2018-02-26 10:52:04 +0300

    Добавьте файлы проекта.

---
+++ MyMetroWindowThreadThemeTest/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Windows;
+using MahApps.Metro;
+
+namespace MyMetroWindowThreadThemeTest
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ThemeManager.ChangeAppStyle(this,
+                ThemeManager.Accents.First(x => x.Name.Equals("Red")),
+                ThemeManager.GetAppTheme("BaseLight"));
+        }
+
+        private void BtOpenNewWindow_OnClick(object sender, RoutedEventArgs e)
+        {
+            Thread subWindowThread = new Thread(() =>
+            {
+                SubWindow subWindow = new SubWindow();
+                subWindow.ShowDialog();
+            });
+            subWindowThread.SetApartmentState(ApartmentState.STA);
+            subWindowThread.IsBackground = true;
+            subWindowThread.Start();
+        }
+    }
+}
---
+++ MyMetroWindowThreadThemeTest/SubWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using MahApps.Metro;
+
+namespace MyMetroWindowThreadThemeTest
+{
+    public partial class SubWindow
+    {
+        public SubWindow()
+        {
+            InitializeComponent();
+            ThemeManager.ChangeAppStyle(this,
+                ThemeManager.Accents.First(x => x.Name.Equals("Red")),
+                ThemeManager.GetAppTheme("BaseLight"));
+        }
+    }
+}
